d66cf106636a986bc96fb6fd909c362a76855db0
yohann737 <81634240+yohann737@users.noreply.github.com>
2025-03-23 20:43:04 +0100

    improved str method for Chessboard class

--- chessboard.py
+++ chessboard.py
@@ -46,6 +46,7 @@ class Chessboard:
             raise ValueError("init_board not implemented for size != 8")
     
     def __str__(self):
+        """
         res = ""
         for i in range(self.m):
             for j in range(self.m):
@@ -56,7 +57,74 @@ class Chessboard:
                     res += " "
                 res += " "
             res += "\n"
+        return res"
+        """
+        # ANSI color codes
+        RESET = "\033[0m"
+        BG_LIGHT = "\033[48;5;180m"  # Light brown background
+        BG_DARK = "\033[48;5;94m"    # Dark brown background
+        FG_RED = "\033[31;1m"        # Bold red text (for black pieces)
+        FG_WHITE = "\033[37;1m"      # Bold white text (for white pieces)
+        
+        # Size of each square in characters (width x height)
+        #square_width = 5
+        square_width = 3
+        square_height = 3
+        
+        # Column labels
+        column_labels = "    " + "     ".join([chr(97 + j) for j in range(self.m)])
+        
+        # Initialize the result string with column labels
+        res = column_labels + "\n"
+        
+        # For each row of the board
+        for i in range(self.m):
+            # Each square has square_height lines
+            for line in range(square_height):
+                # Add row label on the first line of each row
+                if line == square_height // 2:
+                    res += f" {8-i} "
+                else:
+                    res += "   "
+                
+                # For each column in the row
+                for j in range(self.m):
+                    pos = vector.Vector(i, j)
+                    # Determine background color (alternating)
+                    bg_color = BG_LIGHT if (i + j) % 2 == 0 else BG_DARK
+                    
+                    # Get piece at this position, if any
+                    piece_str = " "
+                    if pos in self.squares:
+                        piece = self.squares[pos]
+                        # Determine text color based on piece color
+                        fg_color = FG_RED if piece.color == "black" else FG_WHITE
+                        piece_str = str(piece)
+                    else:
+                        fg_color = RESET
+                    
+                    # Middle line contains the piece, other lines are empty
+                    if line == square_height // 2:
+                        content = f"  {fg_color}{piece_str}{RESET}  " if pos in self.squares else "     "
+                    else:
+                        content = "     "  # Empty space for top and bottom lines
+                    
+                    # Add the square with appropriate colors
+                    res += f"{bg_color}{content}{RESET}"
+                
+                # Add row label on the first line of each row (right side)
+                if line == square_height // 2:
+                    res += f" {8-i} "
+                
+                res += "\n"
+                
+        # Add column labels at the bottom
+        res += column_labels
+        
         return res
+
+
+
         
 class Piece:
     def __init__(self, color):
